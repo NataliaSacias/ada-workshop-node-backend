83c5d7491eddd6206fac1c0bf58954f12ce8f65e
NataliaSacias <nsacias@gmail.com>
2021-06-30 23:54:24 +0000

    primer version

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
-const server = express();
+const server = express(); //creo el servidor
 const port = 3000;
 
-server.use(cors());
-server.use(express.urlencoded({ extended: true }));
-server.use(express.json());
+server.use(cors()); //use la libreria de cors
+server.use(express.urlencoded({ extended: true })); //
+server.use(express.json()); //que todas las respuestas me los transforme en json
 
-server.use('/', routes.mainRoutes);
+server.use('/', routes.mainRoutes); //use las rutas, rutas principales
 
 server.listen(port, function() {
     console.log(`server listening on port ${port}`);
-});
+}); //levanto el servidor
